e8af82213064af73b0e9afc118c07bd2c6d2cf70
sunlu <sunlu@innobuddy.com>
2018-08-01 16:13:29 +0800

    examples update

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import { Button } from '@zhike/ti-ui'
+import styles from './styles';
 import './App.css';
 
 class App extends Component {
+  onClick () {
+    alert('click action')
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
+
         <header>
           <h1>智课题库组 UI 组件</h1>
         </header>
+
         <content>
           <h2>Button</h2>
-          <Button text="ceshi"></Button>
+
+          <div className="para">theme</div>
+          <div className="para">
+            <Button className={styles.button} text="default"></Button>
+            <Button className={styles.button} text="darken" theme="darken"></Button>
+            <Button className={styles.button} text="hollow" theme="hollow"></Button>
+          </div>
+
+          <div className="para">icon</div>
+          <div className="para">
+            <Button className={styles.button} text="leftIcon" theme="hollow" leftIcon={require('./correct.png')}></Button>
+            <Button className={styles.button} text="rightIcon" theme="hollow" rightIcon={require('./correct.png')}></Button>
+          </div>
+
+          <div className="para">onClick</div>
+          <div className="para">
+            <Button className={styles.button} text="click" theme="hollow" onClick={this.onClick}></Button>
+          </div>
+
+          <div className="para">isAvailable</div>
+          <div className="para">
+            <Button className={styles.button} text="default" isAvailable={false}></Button>
+            <Button className={styles.button} text="darken" theme="darken" isAvailable={false}></Button>
+            <Button className={styles.button} text="hollow" theme="hollow" isAvailable={false}></Button>
+          </div>
         </content>
+
       </div>
     );
   }
---
+++ examples/src/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'aphrodite';
+
+export default StyleSheet.create({
+  button: {
+    marginRight: '5px',
+    ':last-child': {
+      marginRight: '0'
+    }
+  }
+});
